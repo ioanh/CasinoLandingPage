65535de405b13d9084c0835c98afe404aade948f
Yoan Hristov <ioan.xristov@abv.bg>
2022-03-21 00:52:05 +0300

    playing with the data and transforming the json object to an array so I can *ngFor it and display it dynamically

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { api } from '../services/api';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,10 +11,15 @@ export class MainComponent implements OnInit {
 
   constructor(private _api: api) { }
 
+  games: any;
+
   ngOnInit(): void {
-    console.log(this._api.getapi().subscribe((data)=> {
-      console.log(data);
-    }));
+    this._api.getapi().subscribe((data) => {
+      console.log(data.game_list)
+      console.log(Object.entries(data.game_list))
+      this.games = Object.entries(data.game_list);
+      console.log(this.games[0][1])
+    })
   }
 
 }
